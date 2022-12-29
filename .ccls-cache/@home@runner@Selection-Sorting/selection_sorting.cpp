d4d67d732bb0763c09dd1d166c872b99b364f40d
Yağmur Kuyucu <93012096+YagmurKyc@users.noreply.github.com>
2022-12-29 10:31:46 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Selection-Sorting/selection_sorting.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int i,j,num,min,p,temp;
+  int array[20]{};
+  cout << "Enter array's size:" ;
+  cin >> num;
+
+  for (int i=0; i<num;i++){
+    cin >> array[i];
+    
+  }
+  for (int i=0;i< num;i++){
+    cout << array[i]<< "\t";
+  }
+  cout << endl;
+  //Sorting
+  for (int i=0; i< num-1;i++){//sondan bir önceki elemanla eşitliyoruz ki son elemanla da kıyaslama yapabilelim.
+    min= array[i];
+    p=i;
+    for (int j=i+1; j<num;j++){
+      if(array[j]<min){
+        min = array[j];
+        p=j;
+      }
+    }
+    temp = array[i];
+    array[i]=array[p];
+    array[p]= temp;
+  }
+
+  for (int i=0; i<num;i++){
+    cout << array[i] << "\t";
+  }
+  
+  return 0;
+}
